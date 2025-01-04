08796026968d35dfa4f8f33de91656b6e5006c5e
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2025-01-04 00:24:37 -0500

    Add product routes for CRUD operations

---
+++ ecommerce/internal/routes/product_routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/YugenDev/global-mobility-test/internal/handlers"
+	"github.com/labstack/echo/v4"
+)
+
+func ProductRoutes(e *echo.Echo) {
+	handler := handlers.NewProductHandler()
+
+	e.POST("/products", handler.CreateProduct)
+	e.GET("/products", handler.GetAllProducts)
+	e.GET("/products/:id", handler.GetProductByID)
+	e.PUT("/products/:id", handler.UpdateProduct)
+	e.DELETE("/products/:id", handler.DeleteProduct)
+}
