8519ef53484292849e95aca7a443118d97b3dbe9
Juan Esteban Escobar Restrepo <138255902+YugenDev@users.noreply.github.com>
2025-01-04 00:15:30 -0500

    Refactor Product model to include JSON tags and correct description field

--- ecommerce/internal/models/product.go
+++ ecommerce/internal/models/product.go
@@ -1,18 +1,15 @@
 package models
 
 import (
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
+    "time"
 )
 
 type Product struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty"`
-	ProductID   string             `bson:"product_id,omitempty"`
-	Name        string             `bson:"name,omitempty"`
-	Description string             `bson:"descriptsion,omitempty"`
-	Price       float64            `bson:"price,omitempty"`
-	Stock       int                `bson:"stock,omitempty"`
-	CreatedAt   time.Time          `bson:"created_at,omitempty"`
-	UpdatedAt   time.Time          `bson:"updated_at,omitempty"`
+    ProductID   string    `bson:"product_id,omitempty" json:"product_id"`
+    Name        string    `bson:"name,omitempty" json:"name"`
+    Description string    `bson:"description,omitempty" json:"description"`
+    Price       float64   `bson:"price,omitempty" json:"price"`
+    Stock       int       `bson:"stock,omitempty" json:"stock"`
+    CreatedAt   time.Time `bson:"created_at,omitempty" json:"created_at"`
+    UpdatedAt   time.Time `bson:"updated_at,omitempty" json:"updated_at"`
 }
